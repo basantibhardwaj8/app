e39427437e16173062aa2ec3c8b8ec60df888c64
Anil Sharma <basantibhardwaj8@gmail.com>
2024-07-29 14:58:24 +0530

    Normalize line endings

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+ import Login from './components/login/Login';
+import Register from './components/register/Register';
+import Dashboard from './components/dashboard/Dashboard';
+import Home from './components/dashboard/Home';
+import Invoice from './components/dashboard/Invoice';
+import NewInvoice from './components/dashboard/NewInvoice';
+import Settings from './components/dashboard/Settings';
+import InvoiceDetail from './components/dashboard/InvoiceDetail';
+// import Login from './component/login/Login';
+// import Register from './component/register/Register';
+// import Dashboard from './component/dashboard/Dashboard';
+// import Home from './component/dashboard/Home';
+// import Invoice from './component/dashboard/Invoice';
+// import NewInvoice from './component/dashboard/NewInvoice';
+// import Settings from './component/dashboard/Settings';
+// import InvoiceDetail from './component/dashboard/InvoiceDetail';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={< Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard/home" element={<Home />} /> */}
+        <Route path="/dashboard/home" element={<Home />} />
+        {/* <Route path="/dashboard/home" element={<Home />} /> */}
+        <Route path="/dashboard/invoice" element={<Invoice />} />
+        <Route path="/dashboard/newInvoice" element={<NewInvoice />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
+        <Route path="/dashboard/invoiceDetail" element={<InvoiceDetail/>} />
+      </Routes>
+    </Router>
+  
+  );
+}
+
+export default App;
